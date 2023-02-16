646c4bedd2887aee2ad30bcbb9d046640854d0ef
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-02-16 22:18:09 +0100

    Fixed validation

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const button = document.querySelector("#create-account");
 const alert = document.querySelector("#passwords-do-not-match");
 
 button.addEventListener("click", function () {
-  if (password.value != confirmPassword.value) {
-    alert.style.display = "block";
-  } else {
-    alert.style.display = "none";
+  if (password.value.length > 7 && confirmPassword.value.length > 7) {
+    if (password.value != confirmPassword.value) {
+      alert.style.display = "block";
+    } else {
+      alert.style.display = "none";
+    }
   }
 });
